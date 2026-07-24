2b1c5667db1fefa1bfc5ba08bb43d15588eb1637
Chaitanya Kashid <chaitanyak@techvishwaguru.com>
2026-07-24 23:39:02 +0530

    initial commit

---
+++ tripnest-auth-service/src/test/java/com/tripnest/auth/TripnestAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripnest.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripnestAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
