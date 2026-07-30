0a06617e6d6fd4cb828a6cfd7f82ed90ba74599c
Chaitanya Kashid <chaitanyak@techvishwaguru.com>
2026-07-30 18:08:54 +0530

    Add initial TripNest CRUD service scaffold

---
+++ tripnest-crud-service/src/main/java/com/tripnest/crud/TripnestCrudServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tripnest.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripnestCrudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripnestCrudServiceApplication.class, args);
+    }
+}
